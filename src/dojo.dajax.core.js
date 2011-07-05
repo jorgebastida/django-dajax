3baa1bef1913d0342e2eb36462bf8d7848044d3a
Jorge Bastida <me@jorgebastida.com>
2011-07-05 21:58:40 +0100

    New dajaxice-0.2 exception handling

--- src/dojo.dajax.core.js
+++ src/dojo.dajax.core.js
@@ -2,65 +2,60 @@
 var Dajax = {
     process: function(data)
     {
-        if(data==Dajaxice.EXCEPTION){
-            alert('Something went wrong, please reload the page.');
-        }
-        else{
-            dojo.forEach(data, function(elem,i){ 
-            switch(elem.cmd)
-            {
-                case 'alert':
-                    alert(elem.val)
-                break;
-            
-                case 'data':
-                    eval( elem.fun+"(elem.val);" );
-                break;
-            
-                case 'as':
-                    dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] = elem.val; });
-                break;
-        
-                case 'addcc':
-                    dojo.forEach(elem.val,function(e){
-                        dojo.query(elem.id).addClass(e);
-                    });
-                break;
-        
-                case 'remcc':
-                    dojo.forEach(elem.val,function(e){
-                        dojo.query(elem.id).removeClass(e);
-                    });
-                break; 
-        
-                case 'ap':
-                    dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] += elem.val;});
-                break;
-        
-                case 'pp':
-                    dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] = elem.val + e[elem.prop] ;});
-                break;
-        
-                case 'clr':
-                    dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] = ""; });
-                break;
-        
-                case 'red':
-                    window.setTimeout('window.location="'+elem.url+'";',elem.delay);
-                break;
-        
-                case 'js':
-                    eval(elem.val);
-                break;
-        
-                case 'rm':
-                    dojo.forEach(dojo.query(elem.id), "dojo.query(item).orphan();");
-                break;
-        
-                default:
-                    alert('Unknown action!');
-                }
-            });
-        }
+        dojo.forEach(data, function(elem,i){
+        switch(elem.cmd)
+        {
+            case 'alert':
+                alert(elem.val)
+            break;
+
+            case 'data':
+                eval( elem.fun+"(elem.val);" );
+            break;
+
+            case 'as':
+                dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] = elem.val; });
+            break;
+
+            case 'addcc':
+                dojo.forEach(elem.val,function(e){
+                    dojo.query(elem.id).addClass(e);
+                });
+            break;
+
+            case 'remcc':
+                dojo.forEach(elem.val,function(e){
+                    dojo.query(elem.id).removeClass(e);
+                });
+            break;
+
+            case 'ap':
+                dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] += elem.val;});
+            break;
+
+            case 'pp':
+                dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] = elem.val + e[elem.prop] ;});
+            break;
+
+            case 'clr':
+                dojo.forEach(dojo.query(elem.id),function(e){ e[elem.prop] = ""; });
+            break;
+
+            case 'red':
+                window.setTimeout('window.location="'+elem.url+'";',elem.delay);
+            break;
+
+            case 'js':
+                eval(elem.val);
+            break;
+
+            case 'rm':
+                dojo.forEach(dojo.query(elem.id), "dojo.query(item).orphan();");
+            break;
+
+            default:
+                alert('Unknown action!');
+            }
+        });
     }
 };
--- src/prototype.dajax.core.js
+++ src/prototype.dajax.core.js
@@ -4,66 +4,61 @@ Dajax.prototype = {
     initialize: function(){},
     process: function(data)
     {
-        if(data==Dajaxice.EXCEPTION){
-            alert('Something went wrong, please reload the page.');
-        }
-        else{
-            data.each(function(elem){
-            switch(elem.cmd)
-            {
-                case 'alert':
-                    alert(elem.val)
-                break;
-        
-                case 'data':
-                    eval( elem.fun+"(elem.val);" );
-                break;
-        
-                case 'as':
-                    $$(elem.id).each(function(e){e[elem.prop] = elem.val;});
-                break;
-        
-                case 'addcc':
-                    elem.val.each(function(cssclass){
-                        $$(elem.id).each(function(e){ e.addClassName(cssclass);});
-                    });
-                break;
-        
-                case 'remcc':
-                    elem.val.each(function(cssclass){
-                        $$(elem.id).each(function(e){ e.removeClassName(cssclass);});
-                    });
-                break;
-        
-                case 'ap':
-                    $$(elem.id).each(function(e){ e[elem.prop] += elem.val;});
-                break;
-        
-                case 'pp':
-                    $$(elem.id).each(function(e){ e[elem.prop] = elem.val + e[elem.prop];});
-                break;
-        
-                case 'clr':
-                    $$(elem.id).each(function(e){e[elem.prop] = "";});
-                break;
-        
-                case 'red':
-                    window.setTimeout('window.location="'+elem.url+'";',elem.delay);
-                break;
-        
-                case 'js':
-                    eval(elem.val);
-                break;
-        
-                case 'rm':
-                    $$(elem.id).each(function(e){e.remove();});
-                break;
-        
-                default:
-                    alert('Unknown action!');
-                }
-            });
-        }
+        data.each(function(elem){
+        switch(elem.cmd)
+        {
+            case 'alert':
+                alert(elem.val)
+            break;
+
+            case 'data':
+                eval( elem.fun+"(elem.val);" );
+            break;
+
+            case 'as':
+                $$(elem.id).each(function(e){e[elem.prop] = elem.val;});
+            break;
+
+            case 'addcc':
+                elem.val.each(function(cssclass){
+                    $$(elem.id).each(function(e){ e.addClassName(cssclass);});
+                });
+            break;
+
+            case 'remcc':
+                elem.val.each(function(cssclass){
+                    $$(elem.id).each(function(e){ e.removeClassName(cssclass);});
+                });
+            break;
+
+            case 'ap':
+                $$(elem.id).each(function(e){ e[elem.prop] += elem.val;});
+            break;
+
+            case 'pp':
+                $$(elem.id).each(function(e){ e[elem.prop] = elem.val + e[elem.prop];});
+            break;
+
+            case 'clr':
+                $$(elem.id).each(function(e){e[elem.prop] = "";});
+            break;
+
+            case 'red':
+                window.setTimeout('window.location="'+elem.url+'";',elem.delay);
+            break;
+
+            case 'js':
+                eval(elem.val);
+            break;
+
+            case 'rm':
+                $$(elem.id).each(function(e){e.remove();});
+            break;
+
+            default:
+                alert('Unknown action!');
+            }
+        });
     }
 };
 
