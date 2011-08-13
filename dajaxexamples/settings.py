510a6c3e267ce7b2a5423a7e8bcca191956247ed
Jorge Bastida <me@jorgebastida.com>
2011-08-13 18:27:52 +0100

    Update TEMPLATE_LOADERS to Django 1.3

--- dajaxexamples/settings.py
+++ dajaxexamples/settings.py
@@ -52,8 +52,8 @@ SECRET_KEY = 'z!e&+te4u6lsv%$^mpq6j#&lav23h)h$b^6hh1x)f-2+f_i)$v'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
     'django.template.loaders.eggs.load_template_source',
 )
 
