f249ba8d93668616e343cb3f33ef7242c3d9c360
Jorge Bastida <me@jorgebastida.com>
2011-08-25 18:52:53 +0100

    Add request context_processor to TEMPLATE_CONTEXT_PROCESSORS

--- dajaxexamples/settings.py
+++ dajaxexamples/settings.py
@@ -82,6 +82,14 @@ INSTALLED_APPS = (
     'apiexamples',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+                               "django.core.context_processors.debug",
+                               "django.core.context_processors.i18n",
+                               "django.core.context_processors.media",
+                               "django.core.context_processors.static",
+                               "django.core.context_processors.request",
+                               "django.contrib.messages.context_processors.messages")
+
 GOOGLE_API_KEY = "YOUR_API"
 
 DAJAXICE_MEDIA_PREFIX = "dajax"  # http://domain.com/dajax/
